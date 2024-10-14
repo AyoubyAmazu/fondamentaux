497d6efd3727fcf0c29ed23eb6a6065fe5651001
AyoubyAmazu <ayoubamazu@gmail.com>
2024-10-14 12:39:52 +0100

    tuto1-1 done

---
+++ tuto/app/src/main/java/com/example/tuto/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.tuto
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tuto.ui.theme.TutoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TutoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    val trafficLightColor = "Black"
+
+    val message =
+        if (trafficLightColor == "Red") "Stop"
+        else if (trafficLightColor == "Yellow") "Slow"
+        else if (trafficLightColor == "Green") "Go"
+        else "Invalid traffic-light color"
+
+    println(message)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TutoTheme {
+        Greeting()
+    }
+}
